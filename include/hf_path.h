a49842d97557939e6e3daed34f8ee17f31003264
Andrei Rafael Brongel <abrongel@gmail.com>
2023-06-04 10:37:09 -0300

    project builds again oops

--- include/hf_path.h
+++ include/hf_path.h
@@ -2,8 +2,9 @@
 #define HF_PATH_H
 
 #include <stdbool.h>
+#include <stddef.h>
 
-bool hf_path_normalize(const char* path);
+bool hf_path_normalize(char* buffer, size_t buffer_size, const char* path);
 bool hf_path_exists(const char* path);
 
 bool hf_path_is_directory(const char* path);
--- src/hf_path.c
+++ src/hf_path.c
@@ -2,10 +2,23 @@
 
 char preferred_separator(void);
 
-bool hf_path_normalize(char* path) {
+bool hf_path_normalize(char* buffer, size_t buffer_size, const char* path) {
     char separator = preferred_separator();
+    (void)separator;// TODO:
+    (void)buffer;
 
-    return true;
+    size_t index = 0;
+    while(index < (buffer_size - 1) && *path) {
+        switch(*path) {
+            case '.': // TODO:
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    return !(*path);
 }
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) || defined(__WIN32__)
